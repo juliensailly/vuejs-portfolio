474c9790af117f60834692c4de0b57d9ef6e673f
Julien Sailly <juliensailly76@gmail.com>
2024-02-04 19:23:39 +0100

    style: light mode implemented on footer and navbar

--- vue-js-tutorial/src/components/header-component.vue
+++ vue-js-tutorial/src/components/header-component.vue
@@ -1,12 +1,14 @@
 <template>
   <header>
-    <nav
-      class="dark:bg-gray-900 bg-white sticky w-full z-20 top-0 start-0"
-    >
+    <nav class="dark:bg-gray-900 bg-blue sticky w-full z-20 top-0 start-0">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <router-link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="/pp.jpg" class="h-8 rounded-[50%]" alt="Julien Sailly's profile picture" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          <img
+            src="/pp.jpg"
+            class="h-8 rounded-[50%] border border-white"
+            alt="Julien Sailly's profile picture"
+          />
+          <span class="self-center text-2xl font-semibold whitespace-nowrap text-white"
             >Julien Sailly</span
           >
         </router-link>
@@ -14,14 +16,14 @@
           <a
             href="/CV_Julien_SAILLY.pdf"
             target="_blank"
-            class="text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2 text-center dark:bg-blue-300 dark:hover:bg-blue-400 dark:focus:ring-blue-500"
+            class="text-white bg-blue-100 hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-400 font-medium rounded-lg px-4 py-2 text-center dark:bg-blue-400 dark:hover:bg-blue-300 dark:focus:ring-blue-500"
           >
             Mon CV
-        </a>
+          </a>
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
-            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-sticky"
             aria-expanded="false"
           >
@@ -48,13 +50,15 @@
           id="navbar-sticky"
         >
           <ul
-            class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+            class="flex flex-col gap-2 p-4 md:p-0 mt-4 font-medium rounded-lg bg-blue-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900"
           >
             <li>
-              <router-link to="/" class="block py-2 px-3 rounded md:p-0">Accueil</router-link>
+              <router-link to="/" class="block py-2 px-3 rounded md:p-1">Accueil</router-link>
             </li>
             <li>
-              <router-link to="/projects" class="block py-2 px-3 rounded md:p-0">Projets</router-link>
+              <router-link to="/projects" class="block py-2 px-3 rounded md:p-1"
+                >Projets</router-link
+              >
             </li>
           </ul>
         </div>
