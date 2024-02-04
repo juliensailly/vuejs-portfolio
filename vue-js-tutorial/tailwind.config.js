fdad21fb62eff4abf542a844527e70d3c38eb98a
Julien Sailly <juliensailly76@gmail.com>
2024-02-04 18:04:45 +0100

    feat: footer added + website layout improved

--- vue-js-tutorial/tailwind.config.js
+++ vue-js-tutorial/tailwind.config.js
@@ -16,6 +16,7 @@ export default {
         '4xl': '2rem'
       },
       colors: {
+        // https://coolors.co/57b8ff-e6e6e6-fbb13c-fe6847
         blue: {
           DEFAULT: '#0078cd',
           100: '#002844',
