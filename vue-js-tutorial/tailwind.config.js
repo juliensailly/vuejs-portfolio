f45ba0e1975fd203f879dc70ab05034c8c380e5c
Julien Sailly <juliensailly76@gmail.com>
2024-02-04 15:00:40 +0100

    style: configuration of tailwind

---
+++ vue-js-tutorial/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    colors: {
+      blue: '#2176AE',
+      lightBlue: '#57B8FF',
+      white: '#E6E6E6',
+      yellow: '#FBB13C',
+      red: '#FE6847'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif']
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem'
+      },
+      borderRadius: {
+        '4xl': '2rem'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
