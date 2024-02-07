5e0d3c8a10b1b47e631f07d84cce36f7bc063fc8
Julien Sailly <juliensailly76@gmail.com>
2024-02-07 01:00:28 +0100

    style: adding custom backgrounds depending on the view

--- vue-js-tutorial/src/App.vue
+++ vue-js-tutorial/src/App.vue
@@ -1,15 +1,17 @@
-<script setup>
+<script>
 import headerComponent from './components/header-component.vue'
 import footerComponent from './components/footer-component.vue'
+
+export default {
+  components: { headerComponent, footerComponent }
+}
 </script>
 
 <template>
   <div class="flex flex-col justify-between min-h-[100vh]">
     <headerComponent></headerComponent>
-    <main class="flex-1 bg-white dark:bg-gray-800 text-gray-900 dark:text-white bg-[url('/background/light-topography.svg')] dark:bg-[url('/background/dark-topography.svg')]">
-      <div class="max-w-screen-xl m-auto">
-        <router-view></router-view>
-      </div>
+    <main class="flex-1 flex">
+      <router-view></router-view>
     </main>
     <footerComponent></footerComponent>
   </div>
--- vue-js-tutorial/src/views/ProjectsView.vue
+++ vue-js-tutorial/src/views/ProjectsView.vue
@@ -35,67 +35,73 @@ export default {
 </script>
 
 <template>
-  <div class="text-right px-4 pt-4 md:px-8 md:pt-8">
-    <button
-      id="dropdownDefaultButton"
-      data-dropdown-toggle="dropdown"
-      class="group text-white bg-blue-100 hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-400 dark:hover:bg-blue-300 dark:focus:ring-blue-500"
-      type="button"
-    >
-      {{ sortBtnLabel }}
-      <svg
-        class="w-2.5 h-2.5 ms-3 group-hover:translate-y-0.5 transition-transform duration-300 ease-in-out"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 10 6"
-      >
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="m1 1 4 4 4-4"
-        />
-      </svg>
-    </button>
+  <div
+    class="flex-1 bg-white dark:bg-gray-800 text-gray-900 dark:text-white bg-[url(/background/wiggle-light.svg)] dark:bg-[url(/background/wiggle-dark.svg)]"
+  >
+    <div class="max-w-screen-xl m-auto">
+      <div class="text-right px-4 pt-4 md:px-8 md:pt-8">
+        <button
+          id="dropdownDefaultButton"
+          data-dropdown-toggle="dropdown"
+          class="group text-white bg-blue-100 hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-400 dark:hover:bg-blue-300 dark:focus:ring-blue-500"
+          type="button"
+        >
+          {{ sortBtnLabel }}
+          <svg
+            class="w-2.5 h-2.5 ms-3 group-hover:translate-y-0.5 transition-transform duration-300 ease-in-out"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 1 4 4 4-4"
+            />
+          </svg>
+        </button>
 
-    <div
-      id="dropdown"
-      class="text-left z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-    >
-      <ul
-        class="py-2 text-sm text-gray-700 dark:text-gray-200"
-        aria-labelledby="dropdownDefaultButton"
-      >
-        <li
-          @click="sortedOption = 'relevant'"
-          class="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+        <div
+          id="dropdown"
+          class="text-left z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
         >
-          {{ textData.fr.pages.projects.sort.relevant }}
-        </li>
-        <li
-          @click="sortedOption = 'recent'"
-          class="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-        >
-          {{ textData.fr.pages.projects.sort.recent }}
-        </li>
-        <li
-          @click="sortedOption = 'alphabetical'"
-          class="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-        >
-          {{ textData.fr.pages.projects.sort.alphabetical }}
-        </li>
-      </ul>
+          <ul
+            class="py-2 text-sm text-gray-700 dark:text-gray-200"
+            aria-labelledby="dropdownDefaultButton"
+          >
+            <li
+              @click="sortedOption = 'relevant'"
+              class="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              {{ textData.fr.pages.projects.sort.relevant }}
+            </li>
+            <li
+              @click="sortedOption = 'recent'"
+              class="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              {{ textData.fr.pages.projects.sort.recent }}
+            </li>
+            <li
+              @click="sortedOption = 'alphabetical'"
+              class="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              {{ textData.fr.pages.projects.sort.alphabetical }}
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="flex gap-4 md:gap-8 flex-wrap justify-center p-4 md:p-8">
+        <ProjectCard
+          class="flex-[100%] md:flex-[40%]"
+          v-for="project in sortedTextData"
+          :key="project"
+          :project="project"
+        ></ProjectCard>
+      </div>
     </div>
-  </div>
-
-  <div class="flex gap-4 md:gap-8 flex-wrap justify-center p-4 md:p-8">
-    <ProjectCard
-      class="flex-[100%] md:flex-[40%]"
-      v-for="project in sortedTextData"
-      :key="project"
-      :project="project"
-    ></ProjectCard>
   </div>
 </template>
