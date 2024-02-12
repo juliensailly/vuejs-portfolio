70142f298489efa41f8871813bc013b932cc938e
Julien Sailly <juliensailly76@gmail.com>
2024-02-12 21:53:52 +0100

    fix: responsive bug with tooltip boxes

--- vue-js-tutorial/src/components/github-contributors-component.vue
+++ vue-js-tutorial/src/components/github-contributors-component.vue
@@ -8,6 +8,16 @@ const props = defineProps({
   }
 })
 
+setInterval(() => {
+  var tooltip = document.querySelectorAll('.tooltip')
+  tooltip.forEach((el) => {
+    if (el.classList.contains('invisible')) {
+      el.style.position = 'absolute'
+      el.style.transform = 'translate3d(0px, 0px, 0px)'
+    }
+  })
+}, 1000)
+
 const contributorsData = textData.fr.assets.contributors
 </script>
 
@@ -30,7 +40,7 @@ const contributorsData = textData.fr.assets.contributors
       <div
         :id="contributor"
         role="tooltip"
-        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-800"
       >
         {{ contributorsData[contributor].name }}
         <div class="tooltip-arrow" data-popper-arrow></div>
