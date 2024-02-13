e80fee3a0ec54f6d4e0df6a51e4aea481876a32d
Julien Sailly <juliensailly76@gmail.com>
2024-02-13 23:38:50 +0100

    style: improvements on skill display

--- vue-js-tutorial/src/components/homepage/skills-component.vue
+++ vue-js-tutorial/src/components/homepage/skills-component.vue
@@ -24,16 +24,16 @@ onMounted(() => {
       {{ textData.fr.pages.homepage.skillsTitle }}
     </h2>
     <div class="flex flex-col">
-      <div>
+      <div class="">
         <h3 class="mb-2 text-xl font-sans font-semibold">
           {{ textData.fr.pages.homepage.skills.hard.title }}
         </h3>
-        <div class="md:flex">
+        <div class="flex flex-col md:flex-row gap-4 items-stretch">
           <ul
             id="hard-skills-tab"
             data-tabs-toggle="#hard-skills-tab-content"
             role="tablist"
-            class="flex-column space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0"
+            class="flex flex-col gap-4 text-sm font-medium text-gray-900 dark:text-gray-400"
           >
             <li v-for="skillType in hardSkills" :key="skillType.id">
               <button
@@ -41,7 +41,7 @@ onMounted(() => {
                 :data-tabs-target="'#' + skillType.id"
                 type="button"
                 role="tab"
-                class="inline-flex items-center px-4 py-3 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600"
+                class="inline-flex items-center px-4 py-3 bg-blue-700 hover:bg-blue-600 hover:text-gray-900 dark:text-gray-900 text-gray-900 rounded-lg active w-full dark:bg-blue-600 dark:hover:bg-blue-500 aria-selected:bg-blue-500 aria-selected:hover:bg-blue-500 aria-selected:hover:text-gray-900 hover:dark:text-gray-900 aria-selected:dark:bg-blue-400 aria-selected:dark:hover:bg-blue-400 aria-selected:dark:hover:text-gray-900"
               >
                 <img
                   :src="skillType.iconURL"
@@ -52,25 +52,27 @@ onMounted(() => {
               </button>
             </li>
           </ul>
-          <div id="hard-skills-tab-content">
+          <div id="hard-skills-tab-content" class="flex-1 bg-blue-700 dark:bg-blue-800 rounded-lg">
             <div
               role="tabpanel"
               :id="skillType.id"
               v-for="skillType in hardSkills"
               :key="skillType.id"
-              class="flex flex-wrap gap-4 p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full"
+              class="grid grid-cols-[repeat(auto-fit,minmax(auto,100px))] gap-4 justify-around p-6 mx-auto text-medium text-gray-700 w-full"
             >
               <div
-                class="flex flex-col flex-1 items-center justify-between"
+                class="flex-1 flex bg-white-500 p-2 rounded shadow-lg min-w-28"
                 v-for="skill in skillType.list"
                 :key="skill"
               >
-                <img
-                  :src="techUsed[skill].src"
-                  :alt="techUsed[skill].title + ' icon'"
-                  class="w-12"
-                />
-                <p>{{ techUsed[skill].title }}</p>
+                <a :href="techUsed[skill].url" class="flex-1 flex flex-col items-center justify-between gap-2">
+                  <img
+                    :src="techUsed[skill].src"
+                    :alt="techUsed[skill].title + ' icon'"
+                    class=""
+                  />
+                  <p class="pt-2 border-t border-blue-700 w-full text-center">{{ techUsed[skill].title }}</p>
+                </a>
               </div>
             </div>
           </div>
