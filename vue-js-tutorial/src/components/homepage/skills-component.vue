2d2ad9456fa302bc831f266d5d013024bdf5d57d
Julien Sailly <juliensailly76@gmail.com>
2024-02-13 22:28:19 +0100

    feat: skill tabs works

--- vue-js-tutorial/src/components/homepage/skills-component.vue
+++ vue-js-tutorial/src/components/homepage/skills-component.vue
@@ -1,7 +1,17 @@
 <script setup>
-import { initFlowbite } from 'flowbite';
+import { initFlowbite } from 'flowbite'
 import textData from '../../assets/texts.json'
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
+
+const hardSkills = textData.fr.pages.homepage.skills.hard.list
+var techUsed = {}
+hardSkills.forEach((skillType) => {
+  skillType.id = skillType.title.replaceAll(' ', '-').replaceAll('é', 'e').toLowerCase()
+
+  skillType.list.forEach((tech) => {
+    techUsed[tech] = textData.fr.assets.technologies[tech]
+  })
+})
 
 onMounted(() => {
   initFlowbite()
@@ -15,6 +25,9 @@ onMounted(() => {
     </h2>
     <div class="flex flex-col">
       <div>
+        <h3 class="mb-2 text-xl font-sans font-semibold">
+          {{ textData.fr.pages.homepage.skills.hard.title }}
+        </h3>
         <div class="md:flex">
           <ul
             id="hard-skills-tab"
@@ -22,36 +35,43 @@ onMounted(() => {
             role="tablist"
             class="flex-column space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0"
           >
-            <li>
-              <a
-                id="profile-tab"
-                data-tabs-target="#profile"
+            <li v-for="skillType in hardSkills" :key="skillType.id">
+              <button
+                :aria-selected="skillType.id === hardSkills[0].id"
+                :data-tabs-target="'#' + skillType.id"
+                type="button"
                 role="tab"
-                aria-controls="profile"
                 class="inline-flex items-center px-4 py-3 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600"
-                aria-current="page"
               >
-                <svg
-                  class="w-4 h-4 me-2 text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
-                  />
-                </svg>
-                Profile
-              </a>
+                <img
+                  :src="skillType.iconURL"
+                  :alt="skillType.title + ' illustration icon'"
+                  class="w-4 h-4 me-2"
+                />
+                {{ skillType.title }}
+              </button>
             </li>
           </ul>
-          <div id="hard-skills-content">
+          <div id="hard-skills-tab-content">
             <div
-              id="profile" role="tabpanel" aria-labelledby="profile-tab"
-              class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full"
+              role="tabpanel"
+              :id="skillType.id"
+              v-for="skillType in hardSkills"
+              :key="skillType.id"
+              class="flex flex-wrap gap-4 p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full"
             >
-              <!-- Tab content -->
+              <div
+                class="flex flex-col flex-1 items-center justify-between"
+                v-for="skill in skillType.list"
+                :key="skill"
+              >
+                <img
+                  :src="techUsed[skill].src"
+                  :alt="techUsed[skill].title + ' icon'"
+                  class="w-12"
+                />
+                <p>{{ techUsed[skill].title }}</p>
+              </div>
             </div>
           </div>
         </div>
