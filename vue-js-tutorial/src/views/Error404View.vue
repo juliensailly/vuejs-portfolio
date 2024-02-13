942b46ae856efdb8664b81cf86d3ece8120a895e
Julien Sailly <juliensailly76@gmail.com>
2024-02-13 15:27:20 +0100

    feat: improving 404 error page

--- vue-js-tutorial/src/views/Error404View.vue
+++ vue-js-tutorial/src/views/Error404View.vue
@@ -10,31 +10,44 @@ const checkIfBackUrlIsOnWebsite = () => {
   <div
     class="flex-1 p-4 md:p-8 bg-white dark:bg-gray-800 text-gray-900 dark:text-white bg-[url(/background/wiggle-light.svg)] dark:bg-[url(/background/wiggle-dark.svg)]"
   >
-    <div class="max-w-screen-xl m-auto">
-      <h1 class="text-4xl font-sans font-extrabold dark:text-white">
-        {{ textData.fr.pages.error404.title }}
-      </h1>
-      <p class="project-card mt-4">{{ textData.fr.pages.error404.message }}</p>
-      <p
-        @click="checkIfBackUrlIsOnWebsite() ? $router.back() : $router.push('/')"
-        class="mt-4 cursor-pointer inline-flex gap-2 items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-400 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-500 dark:bg-blue-400 dark:hover:bg-blue-300 dark:focus:ring-blue-600"
-      >
-        {{ textData.fr.pages.error404.button }}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
-          />
-        </svg>
-      </p>
-    </div>
+    <section>
+      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-sm text-center">
+          <h1
+            class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-400 dark:text-blue-400"
+          >
+            404
+          </h1>
+          <p
+            class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white"
+          >
+            {{ textData.fr.pages.error404.message }}
+          </p>
+          <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+            {{ textData.fr.pages.error404.btnLabel }}
+          </p>
+          <p
+            @click="checkIfBackUrlIsOnWebsite() ? $router.back() : $router.push('/')"
+            class="group inline-flex cursor-pointer text-white bg-blue-400 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg gap-2 text-sm px-5 py-3 text-center dark:focus:ring-blue-700 my-4"
+          >
+            {{ textData.fr.pages.error404.button }}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 group-hover:-translate-x-0.5 transition-transform duration-200"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+              />
+            </svg>
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
--- vue-js-tutorial/src/views/HomeView.vue
+++ vue-js-tutorial/src/views/HomeView.vue
@@ -1,9 +1,14 @@
+<script setup>
+import ProjectSelection from '../components/homepage/project-selection-component.vue'
+</script>
+
 <template>
   <div
     class="flex-1 bg-white dark:bg-gray-800 text-gray-900 dark:text-white  bg-[url(/background/topography-light.svg)] dark:bg-[url(/background/topography-dark.svg)]"
   >
-    <div class="max-w-screen-xl m-auto">
-      <h1 class="col-start-3 col-span-2 text-center row-start-3 row-span-1">Home</h1>
+    <div class="max-w-screen-xl m-auto py-4 md:py-8">
+      <h1 class="px-4 md:px-8 text-center">Home</h1>
+      <ProjectSelection class="px-4 md:px-8 bg-gray-900 dark:bg-blue-400"></ProjectSelection>
     </div>
   </div>
 </template>
