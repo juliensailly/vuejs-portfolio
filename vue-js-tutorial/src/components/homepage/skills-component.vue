190dd35f04522d32e4d876c72f445bc2cc56103a
Julien Sailly <juliensailly76@gmail.com>
2024-02-13 18:46:24 +0100

    feat: adding tech logos for skill display

--- vue-js-tutorial/src/components/homepage/skills-component.vue
+++ vue-js-tutorial/src/components/homepage/skills-component.vue
@@ -45,52 +45,13 @@ onMounted(() => {
                 Profile
               </a>
             </li>
-            <li>
-              <a
-                id="text-tab"
-                data-tabs-target="#text"
-                role="tab"
-                aria-controls="text"
-                class="inline-flex items-center px-4 py-3 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600"
-                aria-current="page"
-              >
-                <svg
-                  class="w-4 h-4 me-2 text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
-                  />
-                </svg>
-                Text
-              </a>
-            </li>
           </ul>
           <div id="hard-skills-content">
             <div
               id="profile" role="tabpanel" aria-labelledby="profile-tab"
               class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full"
             >
-              <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">Profile Tab</h3>
-              <p class="mb-2">
-                This is some placeholder content the Profile tab's associated content, clicking
-                another tab will toggle the visibility of this one for the next.
-              </p>
-              <p>The tab JavaScript swaps classes to control the content visibility and styling.</p>
-            </div>
-            <div
-              id="text" role="tabpanel" aria-labelledby="text-tab"
-              class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full"
-            >
-              <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">text Tab</h3>
-              <p class="mb-2">
-                This is some placeholder content the text tab's associated content, clicking
-                another tab will toggle the visibility of this one for the next.
-              </p>
-              <p>The tab JavaScript swaps classes to control the content visibility and styling.</p>
+              <!-- Tab content -->
             </div>
           </div>
         </div>
