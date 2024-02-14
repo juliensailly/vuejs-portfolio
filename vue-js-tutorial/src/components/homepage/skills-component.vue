222b87446ec3019b0549f3e53dede0caada07ad2
Julien Sailly <juliensailly76@gmail.com>
2024-02-14 19:06:57 +0100

    feat: improved tab for skill showcase

--- vue-js-tutorial/src/components/homepage/skills-component.vue
+++ vue-js-tutorial/src/components/homepage/skills-component.vue
@@ -24,35 +24,42 @@ onMounted(() => {
       {{ textData.fr.pages.homepage.skillsTitle }}
     </h2>
     <div class="flex flex-col">
-      <div class="">
+      <div class="p-2 md:p-4 bg-blue-900 dark:bg-blue-100 rounded-xl">
         <h3 class="mb-2 text-xl font-sans font-semibold">
           {{ textData.fr.pages.homepage.skills.hard.title }}
         </h3>
-        <div class="flex flex-col md:flex-row gap-4 items-stretch">
+        <div
+          class="flex flex-col md:flex-row gap-2 md:gap-4 items-stretch"
+        >
           <ul
             id="hard-skills-tab"
             data-tabs-toggle="#hard-skills-tab-content"
             role="tablist"
-            class="flex flex-col gap-4 text-sm font-medium text-gray-900 dark:text-gray-400"
+            class="flex flex-row flex-wrap md:flex-col gap-2 md:gap-4 text-sm font-medium text-gray-900 dark:text-gray-400"
           >
-            <li v-for="skillType in hardSkills" :key="skillType.id">
+            <li v-for="skillType in hardSkills" :key="skillType.id" class="flex-[40%] md:flex-none">
               <button
                 :aria-selected="skillType.id === hardSkills[0].id"
                 :data-tabs-target="'#' + skillType.id"
                 type="button"
                 role="tab"
-                class="inline-flex items-center px-4 py-3 bg-blue-700 hover:bg-blue-600 hover:text-gray-900 dark:text-gray-900 text-gray-900 rounded-lg active w-full dark:bg-blue-600 dark:hover:bg-blue-500 aria-selected:bg-blue-500 aria-selected:hover:bg-blue-500 aria-selected:hover:text-gray-900 hover:dark:text-gray-900 aria-selected:dark:bg-blue-400 aria-selected:dark:hover:bg-blue-400 aria-selected:dark:hover:text-gray-900"
+                class="inline-flex items-center px-4 py-3 bg-blue-700 hover:bg-blue-600 hover:text-gray-900 dark:text-gray-900 
+                text-gray-900 rounded-lg active w-full dark:bg-blue-600 dark:hover:bg-blue-500 aria-selected:bg-blue-500 
+                aria-selected:md:translate-x-7 aria-selected:md:rounded-r-none transition-all duration-200 aria-selected:hover:bg-blue-500 
+                aria-selected:hover:text-gray-900 hover:dark:text-gray-900 aria-selected:dark:bg-blue-400 aria-selected:dark:hover:bg-blue-400 
+                aria-selected:dark:hover:text-gray-900"
               >
                 <img
                   :src="skillType.iconURL"
                   :alt="skillType.title + ' illustration icon'"
                   class="w-4 h-4 me-2"
                 />
-                {{ skillType.title }}
+                <p class="text-start">{{ skillType.title }}</p>
               </button>
             </li>
+            <div class="flex-[40%]"></div>
           </ul>
-          <div id="hard-skills-tab-content" class="flex-1 bg-blue-700 dark:bg-blue-800 rounded-lg">
+          <div id="hard-skills-tab-content" class="flex-1 bg-blue-500 dark:bg-blue-400 rounded-lg">
             <div
               role="tabpanel"
               :id="skillType.id"
@@ -65,13 +72,18 @@ onMounted(() => {
                 v-for="skill in skillType.list"
                 :key="skill"
               >
-                <a :href="techUsed[skill].url" class="flex-1 flex flex-col items-center justify-between gap-2">
+                <a
+                  :href="techUsed[skill].url"
+                  class="flex-1 flex flex-col items-center justify-between gap-2"
+                >
                   <img
                     :src="techUsed[skill].src"
                     :alt="techUsed[skill].title + ' icon'"
-                    class=""
+                    class="flex-1"
                   />
-                  <p class="pt-2 border-t border-blue-700 w-full text-center">{{ techUsed[skill].title }}</p>
+                  <p class="pt-2 border-t border-blue-700 w-full text-center">
+                    {{ techUsed[skill].title }}
+                  </p>
                 </a>
               </div>
             </div>
