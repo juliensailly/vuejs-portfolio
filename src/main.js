b2d8dc7eb1dd50cd50681b28c09ee546620d8a8d
Julien Sailly <juliensailly76@gmail.com>
2024-02-26 01:23:12 +0100

    feat: adding i18n + en langage json

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
+import fr from './assets/locales/fr.json'
+import en from './assets/locales/en.json'
+
+const i18n = createI18n({
+  locale: 'fr',
+  fallbackLocale: 'en',
+  messages: { fr, en }
+})
+
 const app = createApp(App)
 
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
