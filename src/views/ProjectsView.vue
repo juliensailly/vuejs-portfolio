fa95b36962a2240dbe2d148b20911f061eb05395
Julien Sailly <juliensailly76@gmail.com>
2024-02-26 23:08:50 +0100

    feat: projects page completed

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,50 +1,51 @@
 <script setup>
 import { computed, onMounted, ref, watch } from 'vue'
 import ProjectCard from '../components/project-card-component.vue'
-import textData from '../assets/texts.json'
+import fr from '../assets/locales/fr.json'
+import en from '../assets/locales/en.json'
 import _ from 'lodash'
 import { initFlowbite } from 'flowbite'
-import { useI18n } from "vue-i18n";
+import { useI18n } from 'vue-i18n'
 import { watchEffect } from 'vue'
 
-const { t } = useI18n();
+const { t } = useI18n()
+const locale = useI18n().locale
 watchEffect(() => {
-  document.title = t('pages.projects.title');
+  document.title = t('pages.projects.title')
 })
 
 const sortedOption = ref('relevant')
-const sortBtnLabel = ref(
-  textData.fr.pages.projects.sort.title + ' : ' + textData.fr.pages.projects.sort.relevant
-)
+const sortBtnLabel = ref(t('pages.projects.sort.title') + ' : ' + t('pages.projects.sort.relevant'))
 const sortingDropdownBtn = ref(null)
 
 const hideDropdown = () => {
   sortingDropdownBtn.value.click()
 }
 
-const sortedTextData = computed(() => {
-  // sortingDropdown.value.classList.add('hidden')
-  // sortingDropdown.value.classList.remove('block')
-  if (sortedOption.value === 'alphabetical') {
-    return _.orderBy(textData.fr.pages.projects.values, 'title')
-  } else if (sortedOption.value === 'recent') {
-    return _.orderBy(textData.fr.pages.projects.values, 'recentScore', ['desc'])
+const sortedProjects = computed(() => {
+  let values = locale.value == 'fr' ? fr.pages.projects.values : en.pages.projects.values
+  if (sortedOption.value == 'relevant') {
+    return _.orderBy(values, 'relevantScore', ['desc'])
+  } else if (sortedOption.value == 'recent') {
+    return _.orderBy(values, 'recentScore', ['desc'])
   } else {
-    return _.orderBy(textData.fr.pages.projects.values, 'relevantScore', ['desc'])
+    return _.orderBy(values, 'title')
   }
 })
 
-watch(sortedOption, async (oldSort, newSort) => {
-  if (newSort.value === 'alphabetical') {
+watch(sortedOption, (newSort) => {
+  if (newSort === 'alphabetical') {
     sortBtnLabel.value =
-      textData.fr.pages.projects.sort.title + ' : ' + textData.fr.pages.projects.sort.alphabetical
-  } else if (newSort.value === 'recent') {
-    sortBtnLabel.value =
-      textData.fr.pages.projects.sort.title + ' : ' + textData.fr.pages.projects.sort.recent
+      t('pages.projects.sort.title') + ' : ' + t('pages.projects.sort.alphabetical')
+  } else if (newSort === 'recent') {
+    sortBtnLabel.value = t('pages.projects.sort.title') + ' : ' + t('pages.projects.sort.recent')
   } else {
-    sortBtnLabel.value =
-      textData.fr.pages.projects.sort.title + ' : ' + textData.fr.pages.projects.sort.relevant
+    sortBtnLabel.value = t('pages.projects.sort.title') + ' : ' + t('pages.projects.sort.relevant')
   }
+})
+
+watch(locale, () => {
+  sortBtnLabel.value = t('pages.projects.sort.title') + ' : ' + t('pages.projects.sort.relevant')
 })
 
 onMounted(() => {
@@ -96,19 +97,19 @@ onMounted(() => {
               @click="sortedOption = 'relevant'"
               class="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
-              {{ textData.fr.pages.projects.sort.relevant }}
+              {{ $t('pages.projects.sort.relevant') }}
             </li>
             <li
               @click="sortedOption = 'recent'"
               class="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
-              {{ textData.fr.pages.projects.sort.recent }}
+              {{ $t('pages.projects.sort.recent') }}
             </li>
             <li
               @click="sortedOption = 'alphabetical'"
               class="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
-              {{ textData.fr.pages.projects.sort.alphabetical }}
+              {{ $t('pages.projects.sort.alphabetical') }}
             </li>
           </ul>
         </div>
@@ -117,7 +118,7 @@ onMounted(() => {
       <div class="flex gap-4 md:gap-8 flex-wrap justify-center p-4 md:p-8">
         <ProjectCard
           class="flex-[100%] md:flex-[40%] lg:flex-[25%]"
-          v-for="project in sortedTextData"
+          v-for="project in sortedProjects"
           :key="project.id"
           :project="project"
         ></ProjectCard>
