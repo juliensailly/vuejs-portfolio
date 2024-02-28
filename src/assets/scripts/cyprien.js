53551c10f2fc281fc1afceb7c7bfcd2a9b235af6
Julien Sailly <juliensailly76@gmail.com>
2024-02-28 20:51:09 +0100

    Update cyprien.js

--- src/assets/scripts/cyprien.js
+++ src/assets/scripts/cyprien.js
@@ -1,5 +1,9 @@
 function cyprienUI() {
   if (localStorage.getItem('6p') == 'true') {
+    document.querySelector('html').classList.remove('dark')
+    document.documentElement.style.setProperty('color-scheme', 'light');
+    localStorage.theme = 'light'
+    
     document.body.style.filter = 'hue-rotate(112deg)'
     document.querySelectorAll('img').forEach((img) => {
       img.style.filter = 'hue-rotate(-112deg)'
