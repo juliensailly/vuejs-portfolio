2ca6a3973ab1f7ef4a78fba700b0af46dd376fac
Julien Sailly <juliensailly76@gmail.com>
2024-03-10 17:58:16 +0100

    locale bahivour fixed

--- src/assets/scripts/parametersLogic.js
+++ src/assets/scripts/parametersLogic.js
@@ -21,28 +21,28 @@ export function setTheme(newTheme = 'no_theme') {
 export var theme = ref(localStorage.theme || 'dark')
 
 // Language
-export function setLocale(newLocale = "no_locale") {
-  localStorage.locale = (newLocale == 'en' ? 'en' : 'fr')
-  locale.value = (newLocale == 'en' ? 'en' : 'fr')
+export function setLocale(newLocale) {
+  localStorage.locale = newLocale == 'en' ? 'en' : 'fr'
+  locale.value = newLocale == 'en' ? 'en' : 'fr'
 
   if (document.querySelector('select') != null) {
     document.querySelector('select').value = localStorage.locale
   }
 
   const toggleElement = document.getElementById(
-    locale.value == "fr" ? 'toggleEnglish' : 'toggleFrench'
+    locale.value == 'fr' ? 'toggleFrench' : 'toggleEnglish'
   )
   if (toggleElement != null) {
     toggleElement.click()
   } else {
     setTimeout(() => {
-      document.getElementById(locale.value == false ? 'toggleEnglish' : 'toggleFrench').click()
+      document.getElementById(locale.value == 'fr' ? 'toggleFrench' : 'toggleEnglish').click()
     }, 100)
   }
 }
 
-export var locale = ref(localStorage.locale || 'fr')
+export var locale = ref(localStorage.locale == 'en' ? 'en' : 'fr')
 
-watch(locale, (_, oldLocale) => {
-  document.querySelector('html').setAttribute("lang", oldLocale);
+watch(locale, (newLocale) => {
+  document.querySelector('html').setAttribute('lang', newLocale)
 })
